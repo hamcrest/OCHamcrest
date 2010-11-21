2e15ddb26f4e43a6d35e3f55f88f375654018396
Jon Reid <jon.reid@mac.com>
2010-11-21 05:40:02 +0000

    Added iOS example
    git-svn-id: https://hamcrest.googlecode.com/svn/trunk/hamcrest-objectivec@572 2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2

---
+++ Examples/iOSExample/Example.m
@@ -0,0 +1,24 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+#define HC_SHORTHAND
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
+
+@interface Example : SenTestCase
+@end
+
+@implementation Example
+
+- (void) testUsingAssertThat
+{
+    assertThat(@"xx", is(@"xx"));
+    assertThat(@"yy", isNot(@"xx"));
+    assertThat(@"i like cheese", containsString(@"cheese"));
+}
+
+- (void) testUsingNumbers
+{
+    assertThatInt(42, is(equalToInt(42)));
+    assertThatUnsignedShort(6 * 9, isNot(equalToUnsignedShort(42)));
+}
+
+@end
