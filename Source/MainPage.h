e051d4ccb70fc5f008017663073ea6f304e07a1d
Jon Reid <jon.reid@mac.com>
2010-11-30 04:17:40 +0000

    Renamed distribution file from OCHamcrest to OCHamcrest-1.0. Latest marker for 1.0 candidate.
    git-svn-id: https://hamcrest.googlecode.com/svn/trunk/hamcrest-objectivec@640 2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2

--- Source/MainPage.h
+++ Source/MainPage.h
@@ -8,7 +8,7 @@
 
 /**
     @mainpage OCHamcrest
-    
+
     \section contents Contents:
     \li \ref intro
     \li \ref platforms
@@ -16,7 +16,7 @@
     \li \ref tour
     \li \ref sugar
     \li \ref custom
-    
+
 
     \section intro Introduction
 
@@ -31,12 +31,12 @@
     valuable since it continues to pass even when the thing being tested is broken). Having a tool
     that allows you to pick out precisely the aspect under test and describe the values it should
     have, to a controlled level of precision, helps greatly in writing tests that are "just right."
-    Such tests fail when the behaviour of the aspect under test deviates from the expected behaviour,
+    Such tests fail when the behavior of the aspect under test deviates from the expected behavior,
     yet continue to pass when minor, unrelated changes to the behavior are made.
 
 
     \section platforms Cocoa and iOS
-    
+
     OCHamcrest is supported for both Cocoa and iOS development.
 
     \b Cocoa:
@@ -48,7 +48,7 @@
 #define HC_SHORTHAND
 #import <OCHamcrest/OCHamcrest.h>
     \endcode
-    
+
     \b iOS:
     \li Add \b OCHamcrestIOS.framework to your project.
     \li Add \e "-lstdc++" and \e "-ObjC" to your "Other Linker Flags".
@@ -56,14 +56,14 @@
 #define HC_SHORTHAND
 #import <OCHamcrestIOS/OCHamcrestIOS.h>
     \endcode
-    
-    
+
+
     \section firsttest My first OCHamcrest test
-    
+
     We'll start by writing a very simple Xcode unit test, but instead of using OCUnit's
     \c STAssertEqualObjects function, we'll use OCHamcrest's \c assertThat construct and the
     standard set of matchers.
-    
+
     \code
 #import <SenTestingKit/SenTestingKit.h>
 
@@ -84,24 +84,24 @@
 
 @end
     \endcode
-    
-    The \ref assertThat method is a stylized sentence for making a test assertion. In this example,
-    the subject of the assertion is the object \c theBiscuit, which is the first method parameter.
-    The second method parameter is a matcher for \c Biscuit objects, here a matcher that checks one
-    object is equal to another using the \c -isEqual: method. The test passes since the \c Biscuit
-    class defines an \c -isEqual: method.
+
+    The \ref assertThat function is a stylized sentence for making a test assertion. In this
+    example, the subject of the assertion is the object \c theBiscuit, which is the first method
+    parameter. The second method parameter is a matcher for \c Biscuit objects, here a matcher that
+    checks one object is equal to another using the \c -isEqual: method. The test passes since the
+    \c Biscuit class defines an \c -isEqual: method.
 
     OCHamcrest's functions are actually declared with an "HC" package prefix (such as
     \c HC_assertThat and \c HC_equalTo) to avoid name clashes. To make test writing faster and test
     code more legible, shorthand macros are provided if \c HC_SHORTHAND is defined before including
     the OCHamcrest header. For example, instead of writing \ref HC_assertThat, simply write
     \ref assertThat.
-    
-    
+
+
     \section tour A tour of common matchers
-    
+
     OCHamcrest comes with a library of useful matchers. Here are some of the most important ones.
-    
+
     <ul>
     <li>Core</li>
         <ul>
@@ -117,48 +117,48 @@
         </ul>
     <li>Object</li>
         <ul>
-        <li>\ref equalTo - test object equality using -isEqual:</li>
-        <li>\ref hasDescription - test -description</li>
-        <li>\ref instanceOf - test type</li>
-        <li>\ref notNilValue, \ref nilValue - test for nil</li>
-        <li>\ref sameInstance - test object identity</li>
+        <li>\ref equalTo - tests object equality using \c -isEqual:</li>
+        <li>\ref hasDescription - tests whether \c -description satisfies a given matcher</li>
+        <li>\ref instanceOf - tests type</li>
+        <li>\ref notNilValue, \ref nilValue - tests for nil</li>
+        <li>\ref sameInstance - tests object identity</li>
         </ul>
     <li>Collections</li>
         <ul>
-        <li>\ref hasEntry, \ref hasKey, \ref hasValue - test an NSDictionary contains an entry, key
+        <li>\ref hasEntry, \ref hasKey, \ref hasValue - tests that an NSDictionary contains an entry, key
             or value</li>
-        <li>\ref hasItem, \ref hasItems - test a collection contains elements</li>
+        <li>\ref hasItem, \ref hasItems - tests that a collection contains elements</li>
         </ul>
     <li>Number</li>
         <ul>
-        <li>\ref closeTo - test floating point values are close to a given value</li>
+        <li>\ref closeTo - tests that floating point values are close to a given value</li>
         <li>\ref greaterThan, \ref greaterThanOrEqualTo, \ref lessThan, \ref lessThanOrEqualTo -
-            test ordering</li>
+            tests ordering</li>
         </ul>
     <li>Text</li>
         <ul>
-        <li>\ref equalToIgnoringCase - test string equality ignoring case</li>
-        <li>\ref equalToIgnoringWhiteSpace - test string equality ignoring differences in runs of whitespace</li>
-        <li>\ref containsString, \ref endsWith, \ref startsWith - test string matching</li>
+        <li>\ref equalToIgnoringCase - tests string equality ignoring case</li>
+        <li>\ref equalToIgnoringWhiteSpace - tests string equality ignoring differences in runs of whitespace</li>
+        <li>\ref containsString, \ref endsWith, \ref startsWith - tests string matching</li>
         </ul>
     </ul>
 
 
     \section sugar Syntactic sugar
-    
+
     OCHamcrest strives to make your tests as readable as possible. For example, the \ref is matcher
     is a wrapper that doesn't add any extra behavior to the underlying matcher. The following
     assertions are all equivalent:
-    
+
     \code
 assertThat(theBiscuit, equalTo(myBiscuit));
 assertThat(theBiscuit, is(equalTo(myBiscuit)));
 assertThat(theBiscuit, is(myBiscuit));
     \endcode
-    
-    
+
+
     \section custom Writing custom matchers
-    
+
     A key feature of OCHamcrest is its extensibility. See \ref custom-matchers for an example of how
     to write your own matchers.
 
