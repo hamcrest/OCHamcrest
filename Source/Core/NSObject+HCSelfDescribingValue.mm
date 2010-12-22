7c4097267ba5d9df19711c9a3e8f12bc03e6d882
Jon Reid <jon.reid@mac.com>
2010-12-21 22:26:38 -0800

    iOS: Fixed crash that occurred when trying to describe the matchers allOf, anyOf and isIn. Related to -ObjC linker flag.

--- Source/Core/NSObject+HCSelfDescribingValue.mm
+++ Source/Core/NSObject+HCSelfDescribingValue.mm
@@ -20,3 +20,17 @@
 }
 
 @end
+
+
+// iOS: A linker bug prevents -ObjC from loading files from static libraries if the file contains
+// only categories and no classes. The category method above is not loaded, so we crash if it's
+// invoked.
+// This can be solved by specifying the -all_load linker flag, but this is a heavy-handed approach.
+// Instead, we define a dummy class to avoid the linker problem.
+// See http://developer.apple.com/library/mac/#qa/qa2006/qa1490.html
+
+@interface NSObject_HCSelfDescribingValue : NSObject
+@end
+
+@implementation NSObject_HCSelfDescribingValue
+@end
