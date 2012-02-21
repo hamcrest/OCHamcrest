5a8e696e1ce9bb8467a08f6a4886c169b462530c
Jon Reid <jon.reid@mac.com>
2012-02-20 22:49:26 -0800

    Fixed conformsToProtocol documentation

--- Source/Library/Object/HCConformsToProtocol.h
+++ Source/Library/Object/HCConformsToProtocol.h
@@ -22,21 +22,21 @@
 OBJC_EXPORT id<HCMatcher> HC_conformsToProtocol(Protocol *aProtocol);
 
 /**
- conformsToProtocol(aProtocol) -
- Matches if object conforms to a given protocol.
- 
- @param aProtocol  The protocol to compare against as the expected protocol.
- 
- This matcher checks whether the evaluated object conforms to @a aProtocol.
- 
- Example:
- @li @ref conformsToProtocol(@protocol(NSObject))
- 
- (In the event of a name clash, don't \#define @c HC_SHORTHAND and use the synonym
- @c HC_conformsToProtocol instead.)
- 
- @ingroup object_matchers
+    conformsToProtocol(aProtocol) -
+    Matches if object conforms to a given protocol.
+
+    @param aProtocol  The protocol to compare against as the expected protocol.
+
+    This matcher checks whether the evaluated object conforms to @a aProtocol.
+
+    Example:
+    @li @ref conformsToProtocol(\@protocol(NSObject))
+
+    (In the event of a name clash, don't \#define @c HC_SHORTHAND and use the synonym
+    @c HC_conformsToProtocol instead.)
+
+    @ingroup object_matchers
  */
 #ifdef HC_SHORTHAND
-#define conformsToProtocol HC_conformsToProtocol
+    #define conformsToProtocol HC_conformsToProtocol
 #endif
