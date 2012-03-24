78856d8a1a33a2bc9aeca0e64006314b8d68c8ea
Jon Reid <jon.reid@mac.com>
2012-03-23 19:54:03 -0700

    Added coverage support

---
+++ Source/TestSupport/environment.sh
@@ -0,0 +1,4 @@
+export BUILT_PRODUCTS_DIR="/Users/joreid/Library/Developer/Xcode/DerivedData/OCHamcrest-hexipssoqufkxqbhsfznxjgnrajh/Build/Products/Coverage-iphonesimulator"
+export NATIVE_ARCH="i386"
+export OBJECT_FILE_DIR_normal="/Users/joreid/Library/Developer/Xcode/DerivedData/OCHamcrest-hexipssoqufkxqbhsfznxjgnrajh/Build/Intermediates/OCHamcrest.build/Coverage-iphonesimulator/libochamcrest.build/Objects-normal"
+export SRCROOT="/Users/joreid/Development/Hamcrest/OCHamcrest/Source"
