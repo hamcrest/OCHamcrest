a31f2c2133033af55330feb711cfb95e6d37c7b4
Jon Reid <jon.reid@mac.com>
2012-06-09 19:26:26 -0700

    Ignore environment.sh generated for test coverage

--- Source/TestSupport/environment.sh
+++
@@ -1,4 +0,0 @@
-export BUILT_PRODUCTS_DIR="/Users/joreid/Library/Developer/Xcode/DerivedData/OCHamcrest-hexipssoqufkxqbhsfznxjgnrajh/Build/Products/Coverage-iphonesimulator"
-export NATIVE_ARCH="i386"
-export OBJECT_FILE_DIR_normal="/Users/joreid/Library/Developer/Xcode/DerivedData/OCHamcrest-hexipssoqufkxqbhsfznxjgnrajh/Build/Intermediates/OCHamcrest.build/Coverage-iphonesimulator/libochamcrest.build/Objects-normal"
-export SRCROOT="/Users/joreid/Development/Hamcrest/OCHamcrest/Source"
