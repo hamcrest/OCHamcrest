b9d57c289598d402f7d3b05a086c0002563128b7
Jon Reid <jon.reid@mac.com>
2013-01-05 21:47:37 -0800

    Use XcodeCoverage submodule for code coverage

--- Source/TestSupport/cleancov.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source envcov.sh
-
-# Clean out the old data
-"${LCOV_PATH}/lcov" -d "${OBJ_DIR}" --zerocounters
--- Source/TestSupport/envcov.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source envbuild.sh
-
-LCOV_PATH=${SRCROOT}/TestSupport/lcov-1.9/bin
-OBJ_DIR=${OBJECT_FILE_DIR_normal}/${NATIVE_ARCH}
