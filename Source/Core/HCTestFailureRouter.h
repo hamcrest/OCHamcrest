6ac3492abbc8d28aee94d5e57b6897a84022928a
Jon Reid <jon.reid@mac.com>
2013-08-27 10:14:08 -0700

    Expose HCTestFailureRouter and factory, and add documentation.

--- Source/Core/HCTestFailureRouter.h
+++ Source/Core/HCTestFailureRouter.h
@@ -1,8 +1,16 @@
 #import <Foundation/Foundation.h>
 
 
+/**
+    Base class for routing test failure to appropriate test framework.
+ 
+    @ingroup integration
+ */
 @interface HCTestFailureRouter : NSObject
 
+/**
+    Signal test failure at specific location.
+ */
 - (void)signalFailureInTestCase:(id)testCase
                        fileName:(const char *)fileName
                      lineNumber:(int)lineNumber
--- Source/Core/HCTestFailureRouterFactory.h
+++ Source/Core/HCTestFailureRouterFactory.h
@@ -3,8 +3,20 @@
 @class HCTestFailureRouter;
 
 
+/**
+    Factory that creates appropriate HCTestFailureRouter.
+ 
+    @ingroup integration
+ */
 @interface HCTestFailureRouterFactory : NSObject
 
+/**
+    Return HCTestFailureRouter subclass appropriate for given test case.
+ 
+    @param testCase  Test case for arbitrary test runner.
+ 
+    @return HCTestFailureRouter subclass to use.
+ */
 + (HCTestFailureRouter *)routerForTestCaseType:(id)testCase;
 
 @end
