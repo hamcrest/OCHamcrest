bfdf81101e3d6359aff4123ac54a52ec09819392
Jon Reid <jon@qualitycoding.org>
2015-10-25 17:18:09 -0700

    Convert hasValue to static inline

--- Source/Library/Collection/HCIsDictionaryContainingValue.h
+++ Source/Library/Collection/HCIsDictionaryContainingValue.h
@@ -18,9 +18,8 @@ FOUNDATION_EXPORT id HC_hasValue(id valueMatcher);
 
 #ifndef HC_DISABLE_SHORT_SYNTAX
 /*!
- * @abstract hasValue(valueMatcher) -
- * Creates a matcher for NSDictionaries that matches when the examined dictionary contains at least
- * value that satisfies the specified matcher.
+ * @abstract Creates a matcher for NSDictionaries that matches when the examined dictionary contains
+ * at least value that satisfies the specified matcher.
  * @param valueMatcher The matcher to satisfy for the value, or an expected value for <em>equalTo</em> matching.
  * @discussion This matcher works on any collection that has an <code>-allValues</code> method.
  *
@@ -29,11 +28,14 @@ FOUNDATION_EXPORT id HC_hasValue(id valueMatcher);
  *
  * <b>Examples</b><br />
  * <pre>assertThat(myDictionary, hasValue(equalTo(\@"bar")))</pre>
- * <pre>assertThat(myDictionary, hasValue(\@"bar"))<pre>
+ * <pre>assertThat(myDictionary, hasValue(\@"bar"))</pre>
  *
  * <b>Name Clash</b><br />
  * In the event of a name clash, <code>#define HC_DISABLE_SHORT_SYNTAX</code> and use the synonym
  * HC_hasValue instead.
  */
-#define hasValue HC_hasValue
+static inline id hasValue(id valueMatcher)
+{
+    return HC_hasValue(valueMatcher);
+}
 #endif
