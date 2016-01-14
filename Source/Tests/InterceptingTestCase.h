a068e65bfe613f5a702ff1a2879b70646b6fe42a
Jon Reid <jon@qualitycoding.org>
2016-01-13 19:08:07 -0800

    Bump copyright to 2016.

--- Source/Tests/InterceptingTestCase.h
+++ Source/Tests/InterceptingTestCase.h
@@ -1,6 +1,6 @@
 //
 // Created by Jon Reid on 9/15/15.
-// Copyright (c) 2015 hamcrest.org. All rights reserved.
+// Copyright (c) 2016 hamcrest.org. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
