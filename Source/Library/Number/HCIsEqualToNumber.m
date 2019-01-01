cfa579ea66727038dcbc5f60602cb9434e3d195b
Jon Reid <jon@qualitycoding.org>
2019-01-01 09:08:17 -0800

    Bump copyright to 2019

--- Source/Library/Number/HCIsEqualToNumber.m
+++ Source/Library/Number/HCIsEqualToNumber.m
@@ -1,5 +1,5 @@
 //  OCHamcrest by Jon Reid, https://qualitycoding.org/
-//  Copyright 2018 hamcrest.org. See LICENSE.txt
+//  Copyright 2019 hamcrest.org. See LICENSE.txt
 
 #import "HCIsEqualToNumber.h"
 
