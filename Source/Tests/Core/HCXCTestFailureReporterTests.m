a8e1baade7907e85a13d948c895ce9736cdfdb2e
Jon Reid <jon@qualitycoding.org>
2021-01-30 11:19:23 -0800

    Split HCXCTestFailureReporterTests out of MockSenTestCase.m

---
+++ Source/Tests/Core/HCXCTestFailureReporterTests.m
@@ -0,0 +1,102 @@
+//  OCHamcrest by Jon Reid, https://qualitycoding.org
+//  Copyright 2021 hamcrest. See LICENSE.txt
+
+#import <OCHamcrest/HCAssertThat.h>
+
+#import <OCHamcrest/HCIsEqual.h>
+#import <OCHamcrest/HCTestFailure.h>
+#import <objc/runtime.h>
+
+@import XCTest;
+
+
+@interface HCXCTestFailureReporterTests : XCTestCase
+@property (nonatomic, assign) BOOL interceptFailure;
+@property (nonatomic, assign) NSUInteger failureCount;
+@property (nonatomic, copy) NSString *failureDescription;
+@property (nonatomic, copy) NSString *failureFile;
+@property (nonatomic, assign) NSUInteger failureLine;
+@property (nonatomic, assign) BOOL failureExpected;
+@end
+
+@implementation HCXCTestFailureReporterTests
+
+- (void)recordFailureWithDescription:(NSString *)description
+                              inFile:(NSString *)filePath
+                              atLine:(NSUInteger)lineNumber
+                            expected:(BOOL)expected
+{
+    if (!self.interceptFailure)
+        [super recordFailureWithDescription:description inFile:filePath atLine:lineNumber expected:expected];
+    else
+    {
+        self.failureCount += 1;
+        self.failureDescription = description;
+        self.failureFile = filePath;
+        self.failureLine = lineNumber;
+        self.failureExpected = expected;
+    }
+}
+
+- (void)testXCTestCase_WithMatch_ShouldNotRecordFailure
+{
+    self.interceptFailure = YES;
+    assertThat(@0, equalTo(@0));
+    self.interceptFailure = NO;
+    
+    XCTAssertEqual(self.failureCount, 0U);
+}
+
+- (void)testXCTestCase_WithMismatch_ShouldRecordFailure
+{
+    self.interceptFailure = YES;
+    assertThat(@1, equalTo(@0));
+    self.interceptFailure = NO;
+    
+    XCTAssertEqual(self.failureCount, 1U);
+}
+
+- (void)testXCTestCase_WithMismatch_ShouldRecordFailureWithMismatchDescription
+{
+    NSString *expected = @"EXPECTED";
+    NSString *actual = @"ACTUAL";
+    NSString *expectedMessage = @"Expected \"EXPECTED\", but was \"ACTUAL\"";
+    
+    self.interceptFailure = YES;
+    assertThat(actual, equalTo(expected));
+    self.interceptFailure = NO;
+    
+    XCTAssertEqualObjects(expectedMessage, self.failureDescription);
+}
+
+- (void)testXCTestCase_WithMismatch_ShouldRecordFailureWithCurrentFileName
+{
+    self.interceptFailure = YES;
+    assertThat(@1, equalTo(@0));
+    self.interceptFailure = NO;
+    
+    XCTAssertEqualObjects(
+            [NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding],
+            self.failureFile);
+}
+
+- (void)testXCTestCase_WithMismatch_ShouldRecordFailureWithCurrentLineNumber
+{
+    self.interceptFailure = YES;
+    NSUInteger assertLine = __LINE__ + 1;
+    assertThat(@1, equalTo(@0));
+    self.interceptFailure = NO;
+
+    XCTAssertEqual(self.failureLine, assertLine);
+}
+
+- (void)testXCTestCase_WithMismatch_ShouldRecordFailureAsExpectedMeaningAnAssertionFailure
+{
+    self.interceptFailure = YES;
+    assertThat(@1, equalTo(@0));
+    self.interceptFailure = NO;
+
+    XCTAssertTrue(self.failureExpected);
+}
+
+@end
--- Source/Tests/Core/MockSenTestCase.m
+++ Source/Tests/Core/MockSenTestCase.m
@@ -10,103 +10,12 @@
 @import XCTest;
 
 
-@interface MockXCTestCase : XCTestCase
-@property (nonatomic, assign) BOOL interceptFailure;
-@property (nonatomic, assign) NSUInteger failureCount;
-@property (nonatomic, copy) NSString *failureDescription;
-@property (nonatomic, copy) NSString *failureFile;
-@property (nonatomic, assign) NSUInteger failureLine;
-@property (nonatomic, assign) BOOL failureExpected;
-@end
-
-@implementation MockXCTestCase
-
-- (void)recordFailureWithDescription:(NSString *)description
-                              inFile:(NSString *)filePath
-                              atLine:(NSUInteger)lineNumber
-                            expected:(BOOL)expected
-{
-    if (!self.interceptFailure)
-        [super recordFailureWithDescription:description inFile:filePath atLine:lineNumber expected:expected];
-    else
-    {
-        self.failureCount += 1;
-        self.failureDescription = description;
-        self.failureFile = filePath;
-        self.failureLine = lineNumber;
-        self.failureExpected = expected;
-    }
-}
-
-- (void)testXCTestCase_WithMatch_ShouldNotRecordFailure
-{
-    self.interceptFailure = YES;
-    assertThat(@0, equalTo(@0));
-    self.interceptFailure = NO;
-    
-    XCTAssertEqual(self.failureCount, 0U);
-}
-
-- (void)testXCTestCase_WithMismatch_ShouldRecordFailure
-{
-    self.interceptFailure = YES;
-    assertThat(@1, equalTo(@0));
-    self.interceptFailure = NO;
-    
-    XCTAssertEqual(self.failureCount, 1U);
-}
-
-- (void)testXCTestCase_WithMismatch_ShouldRecordFailureWithMismatchDescription
-{
-    NSString *expected = @"EXPECTED";
-    NSString *actual = @"ACTUAL";
-    NSString *expectedMessage = @"Expected \"EXPECTED\", but was \"ACTUAL\"";
-    
-    self.interceptFailure = YES;
-    assertThat(actual, equalTo(expected));
-    self.interceptFailure = NO;
-    
-    XCTAssertEqualObjects(expectedMessage, self.failureDescription);
-}
-
-- (void)testXCTestCase_WithMismatch_ShouldRecordFailureWithCurrentFileName
-{
-    self.interceptFailure = YES;
-    assertThat(@1, equalTo(@0));
-    self.interceptFailure = NO;
-    
-    XCTAssertEqualObjects(
-            [NSString stringWithCString:__FILE__ encoding:NSUTF8StringEncoding],
-            self.failureFile);
-}
-
-- (void)testXCTestCase_WithMismatch_ShouldRecordFailureWithCurrentLineNumber
-{
-    self.interceptFailure = YES;
-    NSUInteger assertLine = __LINE__ + 1;
-    assertThat(@1, equalTo(@0));
-    self.interceptFailure = NO;
-
-    XCTAssertEqual(self.failureLine, assertLine);
-}
-
-- (void)testXCTestCase_WithMismatch_ShouldRecordFailureAsExpectedMeaningAnAssertionFailure
-{
-    self.interceptFailure = YES;
-    assertThat(@1, equalTo(@0));
-    self.interceptFailure = NO;
-
-    XCTAssertTrue(self.failureExpected);
-}
-
-@end
-
-
 @interface GenericTestCase : NSObject
 @end
 
 @implementation GenericTestCase
 @end
+
 
 @interface GenericTestCaseTest : XCTestCase
 @end
