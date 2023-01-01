dbb587d6b1e72ac5f93d2e567988f90cc811f2e8
Jon Reid <jon@qualitycoding.org>
2023-01-01 15:00:51 -0800

    r - bump copyright year

--- Source/Tests/Collection/FakeWithoutCount.m
+++ Source/Tests/Collection/FakeWithoutCount.m
@@ -1,5 +1,5 @@
 // OCHamcrest by Jon Reid, https://qualitycoding.org
-// Copyright 2022 hamcrest.org. https://github.com/hamcrest/OCHamcrest/blob/main/LICENSE.txt
+// Copyright 2023 hamcrest.org. https://github.com/hamcrest/OCHamcrest/blob/main/LICENSE.txt
 // SPDX-License-Identifier: BSD-2-Clause
 
 #import "FakeWithoutCount.h"
