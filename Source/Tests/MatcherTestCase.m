a24b02d38ba0b8ad7552a505f883252f26630975
Jon Reid <jon@qualitycoding.org>
2023-03-07 17:36:07 -0800

    r - restore 'year of first publication' in copyright notice

--- Source/Tests/MatcherTestCase.m
+++ Source/Tests/MatcherTestCase.m
@@ -1,5 +1,5 @@
 // OCHamcrest by Jon Reid, https://qualitycoding.org
-// Copyright 2023 hamcrest.org. https://github.com/hamcrest/OCHamcrest/blob/main/LICENSE.txt
+// Copyright 2008 hamcrest.org. https://github.com/hamcrest/OCHamcrest/blob/main/LICENSE.txt
 // SPDX-License-Identifier: BSD-2-Clause
 
 #import "MatcherTestCase.h"
